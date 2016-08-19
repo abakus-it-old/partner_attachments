e5aabb38e0fc6febd58aaf35f58438efe1805ff0
Bernard Delhez <bernarddelhez@gmail.com>
2016-08-19 15:06:55 +0200

    update version to 9

--- __openerp__.py
+++ __openerp__.py
@@ -1,15 +1,14 @@
 {
     'name': "Attachments on partners",
-    'version': '1.0',
+    'version': '9.0.1.0',
     'depends': ['sale'],
     'author': "Valentin THIRION, AbAKUS it-solutions SARL",
     'website': "http://www.abakusitsolutions.eu",
     'category': 'Sale',
     'description': 
-    """
-    Adds a field for saving attachments on a partner.
+    """Adds a field for saving attachments on a partner.
 
-    This module has been developed by Valentin THIRION @ AbAKUS it-solutions.
+This module has been developed by Valentin THIRION @ AbAKUS it-solutions.
     """,
     'data': ['view/partner_view.xml',],
 }
